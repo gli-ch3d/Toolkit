5f231c2322129fb86297d225bbb6c799a8fbe6d5
Miguel Murray <142169296+gli-ch3d@users.noreply.github.com>
2026-01-06 20:53:52 +0900

    Updated Mod2.py to actually work

--- Modulo 2 Division/Mod2.py
+++ Modulo 2 Division/Mod2.py
@@ -1,74 +1,30 @@
 ### Modulo 2 Division
+
 val1 = 1010101010110010100101000111001000101011001000000011110000001010100000000001011001010000101000000000000000000001010100110011101110001111
 val2 = 1011
 
-if (len(sys.argv)>1):
-  val1=str(sys.argv[1])
+def modulo2_division(dividend, divisor):
+    
+    # Convert to strings for easier manipulation
+    s_dividend = str(dividend)
+    s_divisor = str(divisor)
+    len_dividend = len(s_dividend)
+    len_divisor = len(s_divisor)
 
-if (len(sys.argv)>2):
-  val2=str(sys.argv[2])
+    # Append zeros to the dividend
+    padded_dividend = s_dividend + '0' * (len_divisor - 1)
+    padded_dividend = list(padded_dividend)
+    s_divisor = list(s_divisor)
 
+    # Perform modulo 2 division
+    for i in range(len_dividend):
+        if padded_dividend[i] == '1':  # Only perform division if the leading bit is 1
+            for j in range(len_divisor):
+                # XOR operation
+                padded_dividend[i + j] = '0' if padded_dividend[i + j] == s_divisor[j] else '1'
+    # The remainder is the last (len_divisor - 1) bits
+    remainder = ''.join(padded_dividend[-(len_divisor - 1):])
+    return remainder
 
-def showpoly(a):
-  str1 = ""
-  nobits = len(a)
-  for x in range (0,nobits-2):
-    if (a[x] == '1'):
-      if (len(str1)==0):
-        str1 +="x**"+str(nobits-x-1)	
-      else: 
-        str1 +="+x**"+str(nobits-x-1)
-  if (a[nobits-2] == '1'):
-    if (len(str1)==0):
-      str1 +="x"
-    else:
-      str1 +="+x"
-
-  if (a[nobits-1] == '1'):
-    str1 +="+1"
-  print(str1)
-	
-
-def toList(x):
-  l = []
-  for i in range (0,len(x)):
-    l.append(int(x[i]))
-  return (l)
-
-def toString(x):
-  str1 =""
-  for i in range (0,len(x)):
-    str1+=str(x[i])
-  return (str1)
-
-def divide(val1,val2):
-  a = toList(val1)
-  b = toList(val2)
-  working=""
-  res=""
-  while len(b) <= len(a) and a:
-    if (a[0] == 1):
-      del a[0]
-      for j in range(len(b)-1):
-        a[j] ^= b[j+1]
-      if (len(a)>0):
-        working +=toString(a)
-        res+= "1"
-      else:
-        del a[0]
-        working +=toString(a)
-        res+="0"
-  print("Result is\t",res)
-  print("Remainder is\t",toString(a))
-  return 
-
-print("Binary form:\t",val1," divided by ",val2)
-print("Decimal form:\t",int(val1,2)," divided by ",int(val2,2))
-print("")
-
-showpoly(val1)
-showpoly(val2)
-
-print("\nDivide operation:\n")
-
-divide(val1,val2)
+remainder = modulo2_division(val1, val2)
+print("Remainder:", remainder)
